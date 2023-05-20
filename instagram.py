43f993aef1820a1f44b94b887f32bd7fca66e9de
AbhijeetShankar <abhijeetshankar98@gmail.com>
2023-05-20 23:20:42 +0530

    insta login added

--- instagram.py
+++ instagram.py
@@ -10,3 +10,25 @@ driver= Chrome(executable_path=driver_path)
 
 
 ## IN PROGRESS
+usern="its_abhijeet_17"
+password="abhijeet0651"
+driver_path = r"chromedriver_win32\chromedriver_win32.exe" 
+service=Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+driver.get("https://www.instagram.com/accounts/login/")
+time.sleep(2)
+search_input = driver.find_element(By.XPATH, " //input[@class= '_aa4b _add6 _ac4d'] ")
+#search_input.clear()
+search_input_pass = driver.find_element(By.XPATH,"//input[@aria-label='Password']")
+search_input.send_keys(usern)
+search_input_pass.send_keys(password)
+# time.sleep(5)
+login=driver.find_element(By.XPATH,"//button[@class='_acan _acap _acas _aj1-']").click()
+time.sleep(30)
+chat=driver.find_element(By.XPATH,f"//a[@href='/direct/inbox/?next=%2Fi{usern}%2F']").click()
+time.sleep(5)
+#savelog        //input[@id='f27b0ac91ec7db']
+
+time.sleep(10)
+driver.quit()
