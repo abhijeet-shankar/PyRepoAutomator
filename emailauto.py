214a7b10afe8210f2a7c38f5b5d1e94c9938a39f
AbhijeetShankar <abhijeetshankar98@gmail.com>
2023-05-22 12:03:47 +0530

    email automation added

--- emailauto.py
+++ emailauto.py
@@ -1,10 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver import Chrome
+import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
-
+from selenium.webdriver import Chrome
+from selenium import webdriver
+import pyautogui
 
 usern=input()
 password=input()
@@ -24,21 +25,24 @@ time.sleep(3)
 
 username=driver.find_element(By.XPATH, " //input[@type='email'] ")
 username.send_keys(usern)
-time.sleep(2)
-next1=driver.find_element(By.XPATH, "//span[@jsname='V67aGc']").click()
+pyautogui.press('enter')
+time.sleep(15)
+# next1=driver.find_element(By.XPATH, "//span[@jsname='V67aGc']").click()
+
 password=driver.find_element(By.XPATH, "//input[@type='password']")
 password.send_keys(password)
+pyautogui.press('enter')
 time.sleep(2)
-next2=driver.find_element(By.XPATH, "//span[@jsname='V67aGc']").click()
+# next2=driver.find_element(By.XPATH, "//span[@jsname='V67aGc']").click()
 
-time.sleep(5)
+time.sleep(20)
 
 #compose  //div[@class ='T-I T-I-KE L3']
 # TO: //input[@id =':1io']
 # cc //span[@id =':1gn']
 #bcc //span[@id =':1gk']
 #subj //input[@id =':1bb']
-#meesage  //div[@id =':1a2']
+#message  //div[@id =':1a2']
 # attachment //div[@id =':1f9']
 #send  //div[@id =':1bl']
 #send sched  //div[@id =':1bp']
@@ -53,3 +57,10 @@ to.sendkeys(receiver)
 
 subject= driver.find_element(By.XPATH, "//input[@id =':1bb']")
 subject.send_keys(subject)
+time.sleep(3)
+send=  driver.find_element(By.XPATH, "//div[@id =':1bl']").click()
+time.sleep(5)
+
+driver.quit()
+
+
